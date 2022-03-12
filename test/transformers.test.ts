a4b27d5f7689f6ae117a6da3b8cda181ccfe2a4d
karibash <y.karibayashi@gmail.com>
2022-03-13 02:07:25 +0900

    Make the regex of swc-loader a constant

--- test/transformers.test.ts
+++ test/transformers.test.ts
@@ -1,30 +1,32 @@
 import { createSwcLoader } from '../src/transformers';
 
+const swcLoaderPattern = /swc-loader/;
+
 describe('createSwcLoader', () => {
   it('Pass empty options', () => {
     const rule = createSwcLoader({});
-    expect(rule.loader).toMatch(/swc-loader/);
+    expect(rule.loader).toMatch(swcLoaderPattern);
     expect(rule.options).toEqual({ parseMap: false });
   });
 
   it('Pass options', () => {
     const options = { jsc: { parser: { syntax: 'typescript' } } } as const;
     const rule = createSwcLoader(options);
-    expect(rule.loader).toMatch(/swc-loader/);
+    expect(rule.loader).toMatch(swcLoaderPattern);
     expect(rule.options).toEqual({ parseMap: false, ...options });
   });
 
   it('Pass options with sourceMaps set to false', () => {
     const options = { sourceMaps: false } as const;
     const rule = createSwcLoader(options);
-    expect(rule.loader).toMatch(/swc-loader/);
+    expect(rule.loader).toMatch(swcLoaderPattern);
     expect(rule.options).toEqual({ parseMap: false, ...options });
   });
 
   it('Pass options with sourceMaps set to true', () => {
     const options = { sourceMaps: true } as const;
     const rule = createSwcLoader(options);
-    expect(rule.loader).toMatch(/swc-loader/);
+    expect(rule.loader).toMatch(swcLoaderPattern);
     expect(rule.options).toEqual({ parseMap: true, ...options });
   });
 });
